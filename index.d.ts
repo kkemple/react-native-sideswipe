d9f468a14804647434d33b8948e582efa871ecbe
Ayan Yenbekbay <ayan.yenb@gmail.com>
2018-12-15 19:54:13 +0100

    Add TS typings

---
+++ index.d.ts
@@ -0,0 +1,53 @@
+declare module 'react-native-sideswipe' {
+    import {Animated, FlatListProps, StyleProp, ViewStyle} from 'react-native';
+    import React from 'react';
+  
+    interface GestureState {
+      stateID: number;
+      moveX: number;
+      moveY: number;
+      x0: number;
+      y0: number;
+      dx: number;
+      dy: number;
+      vx: number;
+      vy: number;
+      numberActiveTouches: number;
+    }
+  
+    interface SideSwipeRenderItemInfo<TItem> {
+      itemIndex: number;
+      currentIndex: number;
+      itemCount: number;
+      item: TItem;
+      animatedValue: Animated.Value;
+    }
+  
+    interface SideSwipeProps<TItem> {
+      style?: StyleProp<ViewStyle>;
+      flatListStyle?: StyleProp<ViewStyle>;
+      contentContainerStyle?: StyleProp<ViewStyle>;
+      itemWidth?: number;
+      contentOffset?: number;
+      threshold?: number;
+      useVelocityForIndex?: boolean;
+      useNativeDriver?: boolean;
+      shouldCapture?: (gestureState: GestureState) => boolean;
+      shouldRelease?: (gestureState: GestureState) => boolean;
+      data: FlatListProps<TItem>['data'];
+      extractKey?: FlatListProps<TItem>['keyExtractor'];
+      renderItem?: (
+        info: SideSwipeRenderItemInfo<TItem>,
+      ) => React.ReactElement<any> | null;
+      index?: number;
+      onIndexChange?: (index: number) => any;
+      onEndReached?: FlatListProps<TItem>['onEndReached'];
+      onEndReachedThreshold?: FlatListProps<TItem>['onEndReachedThreshold'];
+    }
+  
+    class SideSwipe<TItem> extends React.Component<SideSwipeProps<TItem>> {}
+  
+    export default SideSwipe;
+    export {SideSwipeRenderItemInfo, SideSwipeProps};
+  }
+  
